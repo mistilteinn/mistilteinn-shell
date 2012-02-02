1eca522358ebe2583443c19e823e2f3cb11fdede
mzp <mzpppp@gmail.com>
2012-02-03 07:58:32 +0900

    gitのサブコマンドが有効かを確認するコマンドを追加 refs #999

--- lib/mistilteinn/git.rb
+++ lib/mistilteinn/git.rb
@@ -12,6 +12,14 @@ module Mistilteinn
         cmd "git rev-parse --show-toplevel"
       end
 
+      def work_tree?()
+        cmd("git rev-parse --is-inside-work-tree") == "true"
+      end
+
+      def command?(cmd)
+        cmd("which git-#{cmd}") != nil
+      end
+
       private
       def cmd(str)
         str = %x(#{str} 2>/dev/null || echo "").strip
