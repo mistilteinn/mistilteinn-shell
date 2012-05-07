ec0bcd6d16abd8f3f65c50986eea10a34d9c436d
SHIMOMURA Sho <graueule@gmail.com>
2012-05-07 22:43:07 +0900

    add info command for redmine refs #1060

---
+++ lib/mistilteinn/cli/info.rb
@@ -0,0 +1,37 @@
+module Mistilteinn::Cli
+  class Info < ::Mistilteinn::Cli::Command
+    name :info
+    desc 'display information of specified ticket'
+
+    def action
+      issue = its.info args
+      return if issue.nil?
+      body = <<-END
+     ID: #{issue['id']}
+Subject: #{issue['name']}
+ Author: #{issue['author']}
+   Date: #{issue['date']}
+      END
+
+      unless issue['body'].empty?
+        body << "\n"
+        body << issue['body']
+      end
+
+      unless issue['journals'].empty?
+        body << "\n\nnotes:\n"
+
+        issue['journals'].each_with_index do |journal, i|
+          unless journal['note'] == ''
+            body << <<-END
+##{i+1} #{journal['name']} #{journal['date']}
+#{journal['note']}
+            END
+          end
+        end
+      end
+
+      puts body
+    end
+  end
+end
